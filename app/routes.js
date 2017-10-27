0a200e3e686ebb15c7d3ed2c0172531b783b50b5
Ubipo <Ubipo.Skippy@gmail.com>
2017-10-27 14:52:37 +0200

    Social login tests

--- app/routes.js
+++ app/routes.js
@@ -43,6 +43,37 @@ module.exports = function(app, passport) {
     }));
 
     // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email'] }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+            passport.authenticate('facebook', {
+                successRedirect : '/profile',
+                failureRedirect : '/'
+            })
+    );
+
+        // =====================================
+    // GOOGLE ROUTES =======================
+    // =====================================
+    // send to google to do the authentication
+    // profile gets us their basic information including their name
+    // email gets their emails
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/'
+            })
+    );
+
+
+    // =====================================
     // PROFILE SECTION =====================
     // =====================================
     // we will want this protected so you have to be logged in to visit
